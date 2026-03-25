46d6054cd8e2c522d9564b551922e56bdde7f6b0
Nishant Gaikwad <nishantgaikwad2007@gmail.com>
2026-03-25 23:28:50 +0530

    Update api.js

--- src/api/api.js
+++ src/api/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const analyzeReport = (formData) => {
-  return axios.post("http://localhost:5000/api/analyze", formData, {
+  return axios.post("https://backendanalyzer.onrender.com/api/analyze", formData, {
     headers: {
       "Content-Type": "multipart/form-data",
     },
